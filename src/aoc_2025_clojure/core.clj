61fdaf20790ccd84fe82c1456f398273010389f0
lopkol <nora.szoke26@gmail.com>
2025-12-01 18:51:14 +0100

    project setup

---
+++ src/aoc_2025_clojure/core.clj
@@ -0,0 +1,7 @@
+(ns aoc-2025-clojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/aoc_2025_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns aoc-2025-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [aoc-2025-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
